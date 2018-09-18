c975be13d3ed3a18cabe71a5c0d65a1e37997d9b
karencalpo <minnkc@gmail.com>
2018-09-17 22:36:12 -0700

    Added icon with scroll to top in home page.

--- src/footer-bottom.js
+++ src/footer-bottom.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 class FooterBottom extends Component {
+
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div className="footer-bottom">
         <div className="wrap">
-          <a href="#" id="toTop" style={{display:'block', color:'red'}}><span id="toTopHover"></span></a>
+          <a href="#" id="toTop" onClick={scroll.scrollToTop()} ><span id="toTopHover"></span></a>
         </div>
       </div>
     );
   };
-
 };
 
 export default FooterBottom;
